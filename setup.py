2388f7ce76b9afc2fde42efe73490363888f3b56
sigma67 <benedikt.putz@stud.uni-regensburg.de>
2020-02-19 14:42:32 +0100

    Add requests to install_requires

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ytmusicapi',
-      version='0.1',
+      version='0.2',
       description='Unofficial YTMusic API',
       long_description=(open('README.md').read()),
       url='https://github.com/sigma67/ytmusicapi',
@@ -9,6 +9,9 @@ setup(name='ytmusicapi',
       author_email='',
       license='MIT',
       packages=find_packages(),
+      install_requires=[
+            'requests >= 2.22'
+      ],
       include_package_data=True,
       zip_safe=False
 )
