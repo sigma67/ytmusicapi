53b4e0bde386b7607d36aecc8ff992f14186792f
sigma67 <benedikt.putz@wiwi.uni-regensburg.de>
2020-02-21 14:32:59 +0100

    Docs: add master_doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,12 @@ release = __version__
 # ones.
 extensions = ["sphinx.ext.autodoc"]
 
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
