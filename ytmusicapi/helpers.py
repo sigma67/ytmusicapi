97a05e1af5d759d062c0861148b7b50145a3e7f1
ide-an <ideanweb@gmail.com>
2020-11-10 01:26:32 +0900

    Extract ordering logic to helper functions

--- ytmusicapi/helpers.py
+++ ytmusicapi/helpers.py
@@ -28,6 +28,22 @@ def prepare_like_endpoint(rating):
         return None
 
 
+def validate_order_parameter(order):
+    orders = ['a_to_z', 'z_to_a', 'recently_added']
+    if order and order not in orders:
+        raise Exception(
+            "Invalid order provided. Please use one of the following orders or leave out the parameter: "
+            + ', '.join(orders))
+
+
+def prepare_order_params(order):
+    orders = ['a_to_z', 'z_to_a', 'recently_added']
+    if order is not None:
+        # determine order_params via `.contents.singleColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[1].itemSectionRenderer.header.itemSectionTabbedHeaderRenderer.endItems[1].dropdownRenderer.entries[].dropdownItemRenderer.onSelectCommand.browseEndpoint.params` of `/youtubei/v1/browse` response
+        order_params = ['ggMGKgQIARAA', 'ggMGKgQIARAB', 'ggMGKgQIABAB']
+        return order_params[orders.index(order)]
+
+
 def html_to_txt(html_text):
     tags = re.findall("<[^>]+>", html_text)
     for tag in tags:
