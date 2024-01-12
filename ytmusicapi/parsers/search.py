16c9cbe40ea2123470c0e82a33ff3930a456a6d4
sigma67 <ytmusicapi@gmail.com>
2024-01-12 19:42:15 +0100

    search: fix browseId for album top results

--- ytmusicapi/parsers/search.py
+++ ytmusicapi/parsers/search.py
@@ -40,7 +40,7 @@ def parse_top_result(data, search_result_types):
         search_result.update(song_info)
 
     if result_type in ["album"]:
-        search_result["browseId"] = nav(data, NAVIGATION_BROWSE_ID, True)
+        search_result["browseId"] = nav(data, TITLE + NAVIGATION_BROWSE_ID, True)
 
     search_result["thumbnails"] = nav(data, THUMBNAILS, True)
     return search_result
