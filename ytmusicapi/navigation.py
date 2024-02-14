9819ac1871098bebe1fbdae93d92267b60c05fdb
Sean Reifschneider <sean@realgo.com>
2024-02-14 07:51:48 -0700

    nav() fix for walking lists.
    This goes back to catching exceptions because the `root` can also
    be a list during the loop lifetime.  Narrowed down to KeyError
    and IndexError exceptions, enrich the exception msg with details
    about the failure.

--- ytmusicapi/navigation.py
+++ ytmusicapi/navigation.py
@@ -93,11 +93,12 @@ def nav(root: Dict, items: List[Any], none_if_absent: Literal[True] = True) -> O
 def nav(root: Dict, items: List[Any], none_if_absent: bool = False) -> Optional[Any]:
     """Access a nested object in root by item sequence."""
     for k in items:
-        if k not in root:
+        try:
+            root = root[k]
+        except (KeyError, IndexError) as e:
             if none_if_absent:
                 return None
-            raise KeyError(f"Unable to find '{k}' in {root!r}")
-        root = root[k]
+            raise type(e)(f"Unable to find '{k}' using path {items!r} on {root!r}, exception: {e}")
     return root
 
 
