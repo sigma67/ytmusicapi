52645abb8f6900e40c1e6b3549ecd8d4884683f1
fischcode <82546407+fischcode@users.noreply.github.com>
2024-02-25 01:50:25 +0100

    Add unit test for playlist "own_playlist" attribut
    Add unit test for playlist "own_playlist" attribut

--- tests/mixins/test_playlists.py
+++ tests/mixins/test_playlists.py
@@ -11,6 +11,7 @@ class TestPlaylists:
         assert len(playlist["duration"]) > 5
         assert len(playlist["tracks"]) > 200
         assert "suggestions" not in playlist
+        assert playlist["own_playlist"] == False
 
         yt.get_playlist("RDATgXd-")
         assert len(playlist["tracks"]) >= 100
@@ -24,6 +25,7 @@ class TestPlaylists:
         assert len(playlist["tracks"]) < 100
         assert len(playlist["suggestions"]) == 21
         assert len(playlist["related"]) == 10
+        assert playlist["own_playlist"] == True
 
     def test_edit_playlist(self, config, yt_brand):
         playlist = yt_brand.get_playlist(config["playlists"]["own"])
