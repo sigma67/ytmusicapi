89c8e2a1e44a019723a4825ff9fead4cfa9de1cd
sigma67 <sigma67.github@gmail.com>
2024-03-07 20:50:50 +0100

    fix #544

--- ytmusicapi/parsers/albums.py
+++ ytmusicapi/parsers/albums.py
@@ -1,6 +1,7 @@
 from ytmusicapi.helpers import to_int
 
 from ._utils import *
+from .podcasts import parse_base_header
 from .songs import parse_like_status, parse_song_runs
 
 
@@ -32,6 +33,39 @@ def parse_album_header(response):
     if not album["audioPlaylistId"]:
         album["audioPlaylistId"] = nav(toplevel, [0, "buttonRenderer", *NAVIGATION_PLAYLIST_ID], True)
     service = nav(toplevel, [1, "buttonRenderer", "defaultServiceEndpoint"], True)
+    if service:
+        album["likeStatus"] = parse_like_status(service)
+
+    return album
+
+
+def parse_album_header_2024(response):
+    header = nav(response, [*TWO_COLUMN_RENDERER, *TAB_CONTENT, *SECTION_LIST_ITEM, *RESPONSIVE_HEADER])
+    album = {
+        "title": nav(header, TITLE_TEXT),
+        "type": nav(header, SUBTITLE),
+        "thumbnails": nav(header, THUMBNAILS),
+        "isExplicit": nav(header, SUBTITLE_BADGE_LABEL, True) is not None,
+    }
+
+    album["description"] = nav(header, ["description", *DESCRIPTION_SHELF, *DESCRIPTION], True)
+
+    album_info = parse_song_runs(header["subtitle"]["runs"][2:])
+    album_info["artists"] = [parse_base_header(header)["author"]]
+    album.update(album_info)
+
+    if len(header["secondSubtitle"]["runs"]) > 1:
+        album["trackCount"] = to_int(header["secondSubtitle"]["runs"][0]["text"])
+        album["duration"] = header["secondSubtitle"]["runs"][2]["text"]
+    else:
+        album["duration"] = header["secondSubtitle"]["runs"][0]["text"]
+
+    # add to library/uploaded
+    buttons = header["buttons"]
+    album["audioPlaylistId"] = nav(
+        buttons, [1, "musicPlayButtonRenderer", "playNavigationEndpoint", *WATCH_PLAYLIST_ID], True
+    )
+    service = nav(buttons, [0, "toggleButtonRenderer", "defaultServiceEndpoint"], True)
     if service:
         album["likeStatus"] = parse_like_status(service)
 
