89f36c7b9193ae7e064cf840b50f08234649803e
Adwait Thattey <coderdude1999@gmail.com>
2024-05-31 11:40:11 +0530

    fix sum_total_duration failing if duration_seconds is not an int
    Signed-off-by: Adwait Thattey <coderdude1999@gmail.com>

--- ytmusicapi/helpers.py
+++ ytmusicapi/helpers.py
@@ -71,4 +71,7 @@ def to_int(string):
 def sum_total_duration(item):
     if "tracks" not in item:
         return 0
-    return sum([track["duration_seconds"] if "duration_seconds" in track else 0 for track in item["tracks"]])
+    return sum([track["duration_seconds"] 
+                    if ("duration_seconds" in track and isinstance(track["duration_seconds"], int))
+                    else 0 for track in item["tracks"]
+                ])
