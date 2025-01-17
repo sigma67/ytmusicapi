e41e1a37d7f5be0ff8a05cd2e6a614b285793486
sigma67 <16363825+sigma67@users.noreply.github.com>
2025-01-17 18:56:07 +0100

    Fix 721 (#725)
    * update dev-dependencies
    
    * fix INP001
    
    * fix #721
    
    * add test
    
    * format

--- tests/test_ytmusic.py
+++ tests/test_ytmusic.py
@@ -1,6 +1,14 @@
+import pytest
+
 from ytmusicapi import YTMusic
+from ytmusicapi.exceptions import YTMusicUserError
 
 
 def test_ytmusic_context():
     with YTMusic(requests_session=False) as yt:
         assert isinstance(yt, YTMusic)
+
+
+def test_ytmusic_auth_error():
+    with pytest.raises(YTMusicUserError, match="Invalid auth"):
+        YTMusic(auth="def")
