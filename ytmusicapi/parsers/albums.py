6be30f0cb20025ce03cf8a3ab8e5d1d49fcd84d3
sigma67 <sigma67.github@gmail.com>
2025-03-05 21:42:02 +0100

    backwards compatibility

--- ytmusicapi/parsers/albums.py
+++ ytmusicapi/parsers/albums.py
@@ -67,6 +67,13 @@ def parse_album_header_2024(response):
         ["musicPlayButtonRenderer", "playNavigationEndpoint", *WATCH_PID],
         True,
     )
+    # remove this once A/B testing is finished and it is no longer covered
+    if album["audioPlaylistId"] is None:
+        album["audioPlaylistId"] = nav(
+            find_object_by_key(buttons, "musicPlayButtonRenderer"),
+            ["musicPlayButtonRenderer", "playNavigationEndpoint", *WATCH_PLAYLIST_ID],
+            True,
+        )
     service = nav(
         find_object_by_key(buttons, "toggleButtonRenderer"),
         ["toggleButtonRenderer", "defaultServiceEndpoint"],
