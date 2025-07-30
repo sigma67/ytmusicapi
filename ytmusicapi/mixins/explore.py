55dd621c513251c152bb5f784887c35130103b16
Solomon Victorino <git@solomonvictorino.com>
2025-07-30 17:23:45 -0600

    get_explore: init

--- ytmusicapi/mixins/explore.py
+++ ytmusicapi/mixins/explore.py
@@ -86,3 +86,158 @@ class ExploreMixin(MixinProtocol):
                 playlists += parse_content_list(results, parse_playlist)
 
         return playlists
+
+    def get_explore(self) -> JsonDict:
+        """
+        Get latest explore data from YouTube Music.
+        The Top Songs chart is only returned when authenticated with a premium account.
+
+        :return: Dictionary containing new album releases, top songs (if authenticated with a premium account), moods & genres, popular episodes, trending tracks, and new music videos.
+
+        Example::
+
+            {
+                "new_releases": [
+                    {
+                        "title": "Hangang",
+                        "type": "Album",
+                        "artists": [
+                            {
+                              "id": "UCpo4SbqmPXpCVA5RFj-Gq5Q",
+                              "name": "Dept"
+                            }
+                        ],
+                        "browseId": "MPREb_rGl39ZNEl95",
+                        "audioPlaylistId": "OLAK5uy_mTZAp8a-agh1at-cVUGrwPhTJoM5GnKTk",
+                        "thumbnails": [...],
+                        "isExplicit": false
+                    }
+                ],
+                "top_songs": {
+                    "playlist": "VLPL4fGSI1pDJn6O1LS0XSdF3RyO0Rq_LDeI",
+                    "items": [
+                        {
+                            "title": "Outside (Better Days)",
+                            "videoId": "oT79YlRtXDg",
+                            "artists": [
+                                {
+                                    "name": "MO3",
+                                    "id": "UCdFt4Cvhr7Okaxo6hZg5K8g"
+                                },
+                                {
+                                    "name": "OG Bobby Billions",
+                                    "id": "UCLusb4T2tW3gOpJS1fJ-A9g"
+                                }
+                            ],
+                            "thumbnails": [...],
+                            "isExplicit": true,
+                            "album": {
+                                "name": "Outside (Better Days)",
+                                "id": "MPREb_fX4Yv8frUNv"
+                            },
+                            "rank": "1",
+                            "trend": "up"
+                        }
+                    ]
+                },
+                "moods_and_genres": [
+                    {
+                        "title": "Chill",
+                        "params": "ggMPOg1uXzVuc0dnZlhpV3Ba"
+                    }
+                ],
+                "top_episodes": [
+                    {
+                        "title": "132. Lean Into Failure: How to Make Mistakes That Work | Think Fast, Talk Smart: Communication...",
+                        "description": "...",
+                        "duration": "25 min",
+                        "videoId": "xAEGaW2my7E",
+                        "browseId": "MPEDxAEGaW2my7E",
+                        "videoType": "MUSIC_VIDEO_TYPE_PODCAST_EPISODE",
+                        "date": "Mar 5, 2024",
+                        "thumbnails": [...],
+                        "podcast": {
+                            "id": "UCGwuxdEeCf0TIA2RbPOj-8g",
+                            "name: "Stanford Graduate School of Business"
+                        }
+                    }
+                ],
+                "trending": {
+                    "playlist": "VLOLAK5uy_kNWGJvgWVqlt5LsFDL9Sdluly4M8TvGkM",
+                    "items": [
+                        {
+                            "title": "Permission to Dance",
+                            "videoId": "CuklIb9d3fI",
+                            "playlistId": "OLAK5uy_kNWGJvgWVqlt5LsFDL9Sdluly4M8TvGkM",
+                            "artists": [
+                                {
+                                    "name": "BTS",
+                                    "id": "UC9vrvNSL3xcWGSkV86REBSg"
+                                }
+                            ],
+                            "thumbnails": [...],
+                            "isExplicit": false,
+                            "views": "108M"
+                        }
+                    ]
+                },
+                "new_videos": [
+                    {
+                        "title": "EVERY CHANCE I GET (Official Music Video) (feat. Lil Baby & Lil Durk)",
+                        "videoId": "BTivsHlVcGU",
+                        "artists": [
+                            {
+                                "name": "DJ Khaled",
+                                "id": "UC0Kgvj5t_c9EMWpEDWJuR1Q"
+                            }
+                        ],
+                        "playlistId": null,
+                        "thumbnails": [...],
+                        "views": "46M"
+                    }
+                ]
+            }
+
+        """
+        body: JsonDict = {"browseId": "FEmusic_explore"}
+
+        response = self._send_request("browse", body)
+        results = nav(response, SINGLE_COLUMN_TAB + SECTION_LIST)
+
+        explore: JsonDict = {}
+
+        for result in results:
+            browse_id = nav(result, CAROUSEL + CAROUSEL_TITLE + NAVIGATION_BROWSE_ID, True)
+            if browse_id is None:
+                continue
+
+            contents = nav(result, CAROUSEL_CONTENTS)
+            match browse_id:
+                case "FEmusic_new_releases_albums":
+                    explore["new_releases"] = parse_content_list(contents, parse_album)
+
+                case "FEmusic_moods_and_genres":
+                    explore["moods_and_genres"] = [
+                        {"title": nav(genre, CATEGORY_TITLE), "params": nav(genre, CATEGORY_PARAMS)}
+                        for genre in nav(result, CAROUSEL_CONTENTS)
+                    ]
+
+                case "FEmusic_top_non_music_audio_episodes":
+                    explore["top_episodes"] = parse_content_list(contents, parse_chart_episode, MMRIR)
+
+                case "FEmusic_new_releases_videos":
+                    explore["new_videos"] = parse_content_list(contents, parse_video, MTRIR)
+
+                case playlist_id if playlist_id.startswith("VLPL"):
+                    explore["top_songs"] = {
+                        "playlist": playlist_id,
+                        "items": parse_content_list(contents, parse_chart_song, MRLIR),
+                    }
+
+                case playlist_id if playlist_id.startswith("VLOLA"):
+                    explore["trending"] = {
+                        "playlist": playlist_id,
+                        "items": parse_content_list(contents, parse_trending_song, MRLIR),
+                    }
+
+        return explore
--- ytmusicapi/parsers/explore.py
+++ ytmusicapi/parsers/explore.py
@@ -1,11 +1,13 @@
 from ytmusicapi.parsers.browsing import *
+from ytmusicapi.parsers.podcasts import *
+from ytmusicapi.parsers.songs import *
 from ytmusicapi.type_alias import JsonDict
 
 TRENDS = {"ARROW_DROP_UP": "up", "ARROW_DROP_DOWN": "down", "ARROW_CHART_NEUTRAL": "neutral"}
 
 
 def parse_chart_song(data: JsonDict) -> JsonDict:
-    parsed = parse_song_flat(data)
+    parsed = parse_trending_song(data)
     parsed.update(parse_ranking(data))
     return parsed
 
@@ -16,6 +18,14 @@ def parse_chart_playlist(data: JsonDict) -> JsonDict:
         "playlistId": nav(data, TITLE + NAVIGATION_BROWSE_ID)[2:],
         "thumbnails": nav(data, THUMBNAIL_RENDERER),
     }
+
+
+def parse_chart_episode(data: JsonDict) -> JsonDict:
+    episode = parse_episode(data)
+    del episode["index"]
+    episode["podcast"] = parse_id_name(nav(data, ["secondTitle", "runs", 0]))
+    episode["duration"] = nav(data, SUBTITLE2, True)
+    return episode
 
 
 def parse_chart_artist(data: JsonDict) -> JsonDict:
@@ -33,19 +43,15 @@ def parse_chart_artist(data: JsonDict) -> JsonDict:
     return parsed
 
 
-def parse_chart_trending(data: JsonDict) -> JsonDict:
+def parse_trending_song(data: JsonDict) -> JsonDict:
     flex_0 = get_flex_column_item(data, 0)
-    artists = parse_song_artists(data, 1)
-    index = get_dot_separator_index(artists)
-    # last item is views for some reason
-    views = None if index == len(artists) else artists.pop()["name"].split(" ")[0]
     return {
         "title": nav(flex_0, TEXT_RUN_TEXT),
         "videoId": nav(flex_0, TEXT_RUN + NAVIGATION_VIDEO_ID, True),
+        **(parse_song_runs(nav(get_flex_column_item(data, 1), TEXT_RUNS))),
         "playlistId": nav(flex_0, TEXT_RUN + NAVIGATION_PLAYLIST_ID, True),
-        "artists": artists,
         "thumbnails": nav(data, THUMBNAILS),
-        "views": views,
+        "isExplicit": nav(data, BADGE_LABEL, True) is not None,
     }
 
 
