e6648a5000714ba0fad6602fc4fdd366863b926c
Solomon Victorino <git@solomonvictorino.com>
2026-04-25 16:22:59 -0600

    don't skip locales with country codes (like zh_CN) in update_mo.sh
    update_po.sh already has this range check

--- ytmusicapi/locales/update_mo.sh
+++ ytmusicapi/locales/update_mo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 for dir in *
 do
-  if [ ${#dir} -eq 2 ];
+  if (( ${#dir} >=2 && ${#dir} <=5 ));
   then
     msgfmt -o ${dir}/LC_MESSAGES/base.mo ${dir}/LC_MESSAGES/base
   fi
