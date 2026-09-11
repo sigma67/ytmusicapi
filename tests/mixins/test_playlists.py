3159a51d01d95de58c55e29bb8951322c9aa173e
Hemanga Kashyap <hemangakashyap28@gmail.com>
2026-09-11 20:32:38 +0530

    fix get_playlist_by_id returns None as id for some playlists

--- tests/mixins/test_playlists.py
+++ tests/mixins/test_playlists.py
@@ -80,6 +80,22 @@ class TestPlaylists:
                     assert isinstance(vote_status["netVoteValue"], int)
                     assert vote_status["status"] in VoteStatus
 
+    def test_get_playlist_button_offset(self, yt, data_path):
+        with open(data_path / "2024_03_get_playlist_public.json", encoding="utf8") as f:
+            mock_response = json.load(f)
+
+        header = (
+            mock_response["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][0]["tabRenderer"]["content"][
+                "sectionListRenderer"
+            ]["contents"][0]["musicResponsiveHeaderRenderer"]
+        )
+        # Shift musicPlayButtonRenderer (e.g. Taste Match playlists have an extra button preceding it)
+        header["buttons"].insert(1, {"tasteMatchButtonRenderer": {}})
+
+        with mock.patch("ytmusicapi.YTMusic._send_request", return_value=mock_response):
+            playlist = yt.get_playlist("RDCLAK5uy_lWy02cQBnTVTlwuRauaGKeUDH3L6PXNxI")
+            assert playlist["id"] == "RDCLAK5uy_lWy02cQBnTVTlwuRauaGKeUDH3L6PXNxI"
+
     @pytest.mark.parametrize(
         "playlist_id, tracks_len, related_len",
         [
